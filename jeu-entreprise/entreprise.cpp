e8294618a0ca149d4768df0b068091d45e11e6cb
Romain Catajar <rcatajar@gmail.com>
2015-11-18 23:01:55 +0100

    prix de vente pour entreprise

--- jeu-entreprise/entreprise.cpp
+++ jeu-entreprise/entreprise.cpp
@@ -4,6 +4,12 @@
 using namespace std;
 
 Entreprise::Entreprise(const std::string &n, float treso):
-    Entite(n, treso)
+    Entite(n, treso){}
 
-{}
+float Entreprise::get_prix_de_vente() const{
+    return prix_de_vente;
+}
+
+void Entreprise::set_prix_de_vente(float x){
+    prix_de_vente = x;
+}
--- jeu-entreprise/entreprise.h
+++ jeu-entreprise/entreprise.h
@@ -5,8 +5,13 @@
 
 class Entreprise : public virtual Entite
 {
+    protected:
+        float prix_de_vente;
     public:
         Entreprise(const std::string &, float);
+
+        float get_prix_de_vente() const;
+        void set_prix_de_vente(float);
 };
 
 #endif // ENTREPRISE_H
