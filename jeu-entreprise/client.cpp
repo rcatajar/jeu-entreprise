20dde60d529854e0534a36ea1dab06d170412a6f
Romain Catajar <rcatajar@gmail.com>
2015-11-22 15:56:33 +0100

    Client: attribut objet - pointeur vers un objet - Objet.proprietaire est aussi un pointeur maintenant

--- jeu-entreprise/client.cpp
+++ jeu-entreprise/client.cpp
@@ -1,9 +1,23 @@
 #include "entite.h"
 #include "client.h"
+#include "objet.h"
 
 using namespace std;
 
 Client::Client(const std::string &n, float treso):
-    Entite(n, treso)
+    Entite(n, treso){
+    // au début, le client n'as pas d'objet
+    // on assigne donc le pointeur NULL
+    //(grosso modo c'est comme None en python)
+    objet = NULL;
+}
 
-{}
+void Client::set_objet(Objet* o){
+    objet = o;
+    // Quand le client s'attribue un objet, on change le proprio dans l'objet
+    objet->set_proprietaire(this);
+}
+
+Objet* Client::get_objet() const{
+    return objet;
+}
--- jeu-entreprise/client.h
+++ jeu-entreprise/client.h
@@ -2,11 +2,18 @@
 #define CLIENT_H
 
 #include "entite.h"
+#include "objet.h"
 
 class Client : public Entite
 {
+protected:
+    // l'objet du client (s'il en a un)
+    // j'utilise un pointeur car l'objet peut être null
+    Objet* objet;
 public:
     Client(const std:: string&, float);
+    void set_objet(Objet*);
+    Objet* get_objet() const;
 };
 
 #endif // CLIENT_H
