f57fc03b5f8e411222022ce84902fddadc1370f8
Romain Catajar <rcatajar@gmail.com>
2015-11-24 14:53:58 +0100

    client: adapte et clean méthode achat

--- jeu-entreprise/client.cpp
+++ jeu-entreprise/client.cpp
@@ -33,55 +33,48 @@ void Client::achat(std::vector <Objet*> objets_a_vendre){
     float norm = 0;
     int index_max = 0;
 
+    for(int i=0; i<n; i++){
+        e[i] = (Entreprise *) objets_a_vendre[i]->get_proprietaire();
+        argent_restant[i] = tresorerie - e[i]->get_prix_de_vente();
 
-    if (n==0){
-        // do nothing
+        if(argent_restant[i] > 0){
+            tableau_preferences[i] = argent_restant[i] * objets_a_vendre[i]->get_qualite();
+            norm += tableau_preferences[i] * tableau_preferences[i];
+        }
+        else{
+            tableau_preferences[i] = 0;
+        }
+
+
     }
-    else{
-        for(int i=0; i<n; i++){
-            e[i] = (Entreprise *) objets_a_vendre[i]->get_proprietaire();
-            argent_restant[i] = tresorerie - e[i]->get_prix_de_vente();
+    if(norm != 0){ // si il y a bien un objet qu'peut acheter la dedans
 
-            if(argent_restant[i] > 0){
-                tableau_preferences[i] = argent_restant[i] * objets_a_vendre[i]->get_qualite();
-                norm += tableau_preferences[i] * tableau_preferences[i];
+        for(int i=0; i<n; i++){ // je boucle sur tous les objets que je peux acheter
+
+            tableau_preferences[i] = tableau_preferences[i] / norm; // = 0 si on n'aimait pas l'objet, >0 sinon
+
+            if(tableau_preferences[i] > tableau_preferences[index_max]){ // Je vais chercher l'objet que je préfère
+                index_max = i;
             }
-            else{
-                tableau_preferences[i] = 0;
-            }
-
-
         }
-        if(norm != 0){ // si il y a bien un objet qu'peut acheter la dedans
 
-            for(int i=0; i<n; i++){ // je boucle sur tous les objets que je peux acheter
+        // je perds l'argent nécéssaire
+        //cout << "avant l'achat : " << this->get_tresorerie() << endl;
+        this->set_tresorerie(argent_restant[index_max]);
+        //cout << "après l'achat : " << this->get_tresorerie() << endl;
 
-                tableau_preferences[i] = tableau_preferences[i] / norm; // = 0 si on n'aimait pas l'objet, >0 sinon
+        // l'entreprise reçoit l'argent que j'ai dépensé
+        //cout << "avant l'achat : " << e[index_max]->get_tresorerie() << endl;
+        e[index_max]->set_tresorerie(e[index_max]->get_tresorerie() + e[index_max]->get_prix_de_vente());
+        //cout << "après l'achat : " << e[index_max]->get_tresorerie() << endl;
 
-                if(tableau_preferences[i] > tableau_preferences[index_max]){ // Je vais chercher l'objet que je préfère
-                    index_max = i;
-                }
-            }
+        // je deviens propriétaire de l'objet et l'ajoute à mon stock
+        //cout << "avant l'achat : " << objets_a_vendre[index_max]->get_proprietaire()->get_nom() << endl;
+        this->ajouter_au_stock(objets_a_vendre[index_max]);
+        //cout << "après l'achat : " << objets_a_vendre[index_max]->get_proprietaire()->get_nom() << endl;
 
-            // je perds l'argent nécéssaire
-            //cout << "avant l'achat : " << this->get_tresorerie() << endl;
-            this->set_tresorerie(argent_restant[index_max]);
-            //cout << "après l'achat : " << this->get_tresorerie() << endl;
-
-            // l'entreprise reçoit l'argent que j'ai dépensé
-            //cout << "avant l'achat : " << e[index_max]->get_tresorerie() << endl;
-            e[index_max]->set_tresorerie(e[index_max]->get_tresorerie() + e[index_max]->get_prix_de_vente());
-            //cout << "après l'achat : " << e[index_max]->get_tresorerie() << endl;
-
-            // je deviens propriétaire de l'objet
-            //cout << "avant l'achat : " << objets_a_vendre[index_max]->get_proprietaire()->get_nom() << endl;
-            objets_a_vendre[index_max]->set_proprietaire(this);
-            //cout << "après l'achat : " << objets_a_vendre[index_max]->get_proprietaire()->get_nom() << endl;
-
-            // l'entreprise perd l'objet de son stock
-            e[index_max]->retirer_au_stock(objets_a_vendre[index_max]);
-
-        }
+        // l'entreprise perd l'objet de son stock
+        e[index_max]->retirer_au_stock(objets_a_vendre[index_max]);
 
     }
 
