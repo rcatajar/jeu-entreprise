56325366d2bc6615b7b295f1f9b3ee81c52467ba
Romain Catajar <rcatajar@gmail.com>
2015-11-29 18:35:38 +0100

    tests entreprise: setup headers and test methods

--- tests/tests_entreprise.h
+++ tests/tests_entreprise.h
@@ -15,6 +15,19 @@ public:
 
 private Q_SLOTS:
     void init(); // appelé automatiquement avant chaque test
+    void test_constructeur_set_ia();
+    void test_prix_de_vente();
+    void test_cout_fixe();
+    void test_cout_variable();
+    void test_investissement_realise();
+    void test_produire();
+    void test_investir();
+    void test_vente_objet();
+    void test_phase_de_marketing_ia();
+    void test_phase_de_production_ia();
+    void test_phase_de_recherche_ia();
+
+
 
 private:
     Entreprise* entreprise;
