f6cc409e00876c6214ae4e395040b54dc3b6517d
Romain Catajar <rcatajar@gmail.com>
2015-11-29 20:16:15 +0100

    tests entreprises

--- tests/tests_entreprise.h
+++ tests/tests_entreprise.h
@@ -16,19 +16,23 @@ public:
 private Q_SLOTS:
     void init(); // appelé automatiquement avant chaque test
     void test_constructeur_set_ia();
+    void test_constructeur_set_qualite_marginale();
+    void test_constructeur_set_max_recherche();
     void test_prix_de_vente();
     void test_cout_fixe();
     void test_cout_variable();
     void test_investissement_realise();
     void test_produire_0_objet();
     void test_produire();
-    void test_investir();
+    void test_investir_pas_de_changement_de_seuil();
+    void test_investir_saut_de_seuil();
+    void test_investir_recherche_maximale();
+    void test_investir_set_investissement_realise();
+    void test_investir_set_tresorerie();
     void test_vente_objet();
     void test_phase_de_marketing_ia();
     void test_phase_de_production_ia();
     void test_phase_de_recherche_ia();
-
-
 
 private:
     Entreprise* entreprise;
