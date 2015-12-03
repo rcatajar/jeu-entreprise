ac52535d4aa50d84956ae4a1d54274b6acdb56b5
Romain Catajar <rcatajar@gmail.com>
2015-12-03 13:35:20 +0100

    Historique attributes + plug MoteurJeu

--- jeu-entreprise/historique.h
+++ jeu-entreprise/historique.h
@@ -1,12 +1,15 @@
 /* Classe encapsulant l'historique des différentes valeurs clés du jeu
  * pour qu'on puisse tracer des jolies graphes pleins de données cools
  *
+ * Pour le jeu:
+ * - tour en cours
+ * - tour max
  * Pour le marché:
  * - nombre total d'objets en vente
  * - prix de vente moyen
  *
  * Pour les Entreprises:
- *  - noms
+ *  - nom joueur
  * - nombre d'IA
  * - stock au début de chaque tour
  * - prix de vente
@@ -27,10 +30,44 @@
 #ifndef HISTORIQUE_H
 #define HISTORIQUE_H
 
+#include <vector>
+#include <string>
+
+
 class Historique
 {
 public:
     Historique();
+
+    // J'ai mis toute les variables en public pour qu'on puisse les modfier directement
+    // (c'est pas top mais c'est plus rapide a faire)
+
+    // Jeu
+    int tour;
+    int tour_max;
+
+    // Marché
+    std::vector <int> objets_en_vente;
+    std::vector <int> prix_de_vente_moyen;
+
+    // Entreprise
+    std::string nom_joueur;
+    int nombre_ias;
+    std::vector <std::vector <int>> stock;
+    std::vector <std::vector <int>> prix_de_vente;
+    std::vector <std::vector <int>> production;
+    std::vector <std::vector <int>> investissement_recherche;
+    std::vector <std::vector <int>> qualite_marginale;
+    std::vector <std::vector <int>> ventes;
+    std::vector <std::vector <int>> tresoreries;
+    std::vector <std::vector <int>> objets_detruits;
+
+    // Client
+    int nombre_clients;
+    int nombre_acheteurs;
+    int objets_achetes;
+    int objets_detruits_client;
+
 };
 
 #endif // HISTORIQUE_H
