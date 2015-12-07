4fa0b031c613993244f3dd54c3188a6b0e40866e
Thibault Kaspi <thibault.kaspi@gmail.com>
2015-12-07 23:50:38 +0100

    GameOver window added

--- jeu-entreprise/gameover.cpp
+++ jeu-entreprise/gameover.cpp
@@ -1,6 +1,7 @@
 #include "gameover.h"
 #include "ui_gameover.h"
 #include "moteurjeu.h"
+#include <numeric>
 
 GameOver::GameOver(QWidget *parent, MoteurJeu* _moteur) :
     QWidget(parent),
@@ -10,10 +11,16 @@ GameOver::GameOver(QWidget *parent, MoteurJeu* _moteur) :
 
     moteur = _moteur;
 
+    QVector<double> ventes = moteur->historique->get_vente();
+    double sum_ventes = std::accumulate(ventes.begin(), ventes.end(), 0);
+
+    QVector<double> qualite = moteur->historique->get_qualite_moyenne();
+    double mean_qualite = std::accumulate(qualite.begin(), qualite.end(), 0) / qualite.size();
+
     ui->lcd_mois->display(moteur->historique->tour);
-    ui->lcd_qualite->display(moteur->historique->get_qualite_moyenne().back());
+    ui->lcd_qualite->display(mean_qualite);
     ui->lcd_tresorerie->display(moteur->historique->get_tresorerie().back());
-    ui->lcd_ventes->display(moteur->historique->get_vente().back());
+    ui->lcd_ventes->display(sum_ventes);
 }
 
 GameOver::~GameOver()
